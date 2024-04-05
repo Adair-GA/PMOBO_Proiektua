9b991cfd79d07e8829267fbae669dd0288548811
lucia del rio lizaso <ldel-rio@student.42urduliz.com>
2024-04-05 23:14:16 +0100

    Botaren metodoak eginda

--- HelmugakoBagoia/src/packproiektua/Bot.java
+++ HelmugakoBagoia/src/packproiektua/Bot.java
@@ -1,10 +1,27 @@
 package packproiektua;
 
+import java.util.Random;
+
 public class Bot extends Jokalaria {
 	
-	public Bot(String pIzena)
+	private static final String[] izenak = { "Apollo", "DuoLingo", "Hammer", "Golem", "Botfather", "Cosmic", "Silver"};
+	public Bot()
 	{
 		super();
+		izenaEsleitu();
 	}
-
+	
+	private void izenaEsleitu()
+	{
+		Random r = new Random();
+		int randomIndex = r.nextInt(izenak.length);
+		this.izena = izenak[randomIndex];
+	}
+	
+	@Override
+	public void aukeratuBidea(int pBideMax)
+	{
+		Random r = new Random();
+		this.pos = 1 + r.nextInt(pBideMax);
+	}
 }
