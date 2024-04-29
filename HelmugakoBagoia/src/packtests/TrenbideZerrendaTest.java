640003e590d64c4874a5a30bd66200ae86be77ac
MikelEguia <meguiabengoa@gmail.com>
2024-04-29 16:24:54 +0200

    TrenbideZerrenda hecho

--- HelmugakoBagoia/src/packtests/TrenbideZerrendaTest.java
+++ HelmugakoBagoia/src/packtests/TrenbideZerrendaTest.java
@@ -6,26 +6,34 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import packproiektua.TrenbideZerrenda;
+
 class TrenbideZerrendaTest {
+	
+	private TrenbideZerrenda tz;
 	
 	@BeforeEach
 	void setUp() throws Exception{
-		
+		tz=TrenbideZerrenda.getTrenbideZerrenda(3, 2);
 	}
 	
 	@AfterEach
 	void tearDown() throws Exception{
-		
+		tz=null;
 	}
 
 	@Test
 	void testGetTrenbideZerrenda() {
-		fail("Not yet implemented");
+		assertNotNull(tz);
+		TrenbideZerrenda tz2=TrenbideZerrenda.getTrenbideZerrenda(3,2);
+		assertEquals(tz,tz2);
 	}
 
 	@Test
 	void testZatiaLortu() {
-		fail("Not yet implemented");
+		assertNull(tz.zatiaLortu(0));
+		TrenbideZerrenda tz3=TrenbideZerrenda.getTrenbideZerrenda(3, 2);
+		assertNotNull(tz3.zatiaLortu(1));
 	}
 
 }
