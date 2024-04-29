33fa27bf2d37e59f90f66065a5f41966a9df9845
LUC1A05 <ldel-rio@student.42urduliz.com>
2024-04-29 16:13:09 +0200

    trenbideZatia test eginda

--- HelmugakoBagoia/src/packtests/TrenbideZatiaTest.java
+++ HelmugakoBagoia/src/packtests/TrenbideZatiaTest.java
@@ -1,5 +1,6 @@
 package packtests;
 
+import packproiektua.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
@@ -7,9 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TrenbideZatiaTest {
-	
+	private TrenbideBat tb;
+	private TrenbideAsko ta;
 	@BeforeEach
 	void setUp() throws Exception{
+		int i = 3;
+		tb = new TrenbideBat();
+		ta = new TrenbideAsko(i);
 		
 	}
 	
@@ -20,7 +25,10 @@ class TrenbideZatiaTest {
 
 	@Test
 	void testInprimatuZatia() {
-		fail("Not yet implemented");
+		tb.inprimatuZatia();
+		ta.inprimatuZatia(3, 2, 1, false);
+		ta.inprimatuZatia(3, 1, 2, true);
+		ta.inprimatuZatia(10, 5, 3, true);
 	}
 
 }
