46604f9f6c2fd1a718de9bffda02e335ef7a2b70
ainhoo222 <164318917+ainhoo222@users.noreply.github.com>
2024-04-29 16:39:04 +0200

    Add files via upload

---
+++ HelmugakoBagoia/src/packproiektua/TrenbideAskoTest.java
@@ -0,0 +1,50 @@
+package packproiektua;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TrenbideAskoTest {
+
+	@Test
+	void testInprimatuZatia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTrenbideAsko() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetZatia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOztopoaDago() {
+		TrenbideAsko trenbide = new TrenbideAsko(5);
+		int oztopoaPos = trenbide.oztopoaDago();
+		assertTrue(oztopoaPos >= 0 && oztopoaPos < 5);
+	}
+
+	@Test
+	void testInprimatuZatiaIntIntIntBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTxanponaDago() {
+		TrenbideAsko trenbide = new TrenbideAsko(5);
+		int txanponaPos = trenbide.txanponaDago();
+		assertTrue(txanponaPos >= 0 && txanponaPos < 5);
+	}
+
+	@Test
+	void testGetTxanpona() {
+		TrenbideAsko trenbide = new TrenbideAsko(5);
+		int txanponBalioa = trenbide.getTxanpona(trenbide.txanponaDago());
+		assertTrue(txanponBalioa >= 1 && txanponBalioa <= 10);
+	}
+
+}
